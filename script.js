1100e86273a057f39d60026e913c888a6b0b747e
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-09-16 18:18:57 -0700

    Fix not displaying results
    Fix not displaying results after user chose their pick.

--- script.js
+++ script.js
@@ -20,57 +20,53 @@ function playGame(hScore, cScore) {
 
     if (userChoice == 'rock') {
         if (computerChoice == 'scissors') {
-            console.log('Users win!')
+            alert('Users win!');
             humanScore += 1;
         }
         else if (computerChoice == 'paper') {
-            console.log('Computer win!')
+            alert('Computer win!');
             computerScore += 1;
         }
         else {
-            console.log('Tie!')
+            alert('Tie!');
         }
     }
 
     else if (userChoice == 'scissors') {
         if (computerChoice == 'paper') {
-            console.log('Users win!')
+            alert('Users win!');
             humanScore += 1;
         }
         else if (computerChoice == 'rock') {
-            console.log('Computer win!')
+            alert('Computer win!');
             computerScore += 1;
         }
         else {
-            console.log('Tie!')
+            alert('Tie!');
         }
     }
 
     else if (userChoice == 'paper') {
         if (computerChoice == 'rock]') {
-            console.log('Users win!')
+            alert('Users win!');
             humanScore += 1;
         }
         else if (computerChoice == 'scissors') {
-            console.log('Computer win!')
+            alert('Computer win!');
             computerScore += 1;
         }
         else {
-            console.log('Tie!')
+            alert('Tie!');
         }
     }
 
-    console.log('Computer Selection:' + userChoice);
-    console.log('Computer Selection:' + computerChoice);
+    alert('User Selection:' + userChoice + ', Computer Selection:' + computerChoice);
+    alert('Human score:' + humanScore + ', Computer score:' + computerScore);
     
-    return [humanScore, computerScore];
+    return[humanScore, computerScore];
 }
 
 // Play 5 rounds Rock, Paper, and Scissors 
 for (let i = 0; i < 5; i++) {
     scoreUserScoreData = playGame(scoreUserScoreData[0], scoreUserScoreData[1]);
-    console.log('Results:')
-    console.log('Round ' + (i+1) + ': Human score:' + scoreUserScoreData[0]);
-    console.log('Round ' + (i+1) + ': Computer score:' + scoreUserScoreData[1]);
-    console.log('-----------------------------------------------------')
 }
